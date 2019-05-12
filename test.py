43185a453b33c0193d51af88558b152e72f2dbc6
Marzia Favaro <maarzia.f@gmail.com>
2019-05-12 17:33:22 +0200

    First test

---
+++ test.py
@@ -0,0 +1,17 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Images
+import imageio
+
+# Paths
+import os
+
+# PATHS
+project_root = os.path.relpath("..")
+mini_source_root
