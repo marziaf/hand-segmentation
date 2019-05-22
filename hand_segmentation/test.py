acc52e7226671b3f7777ec1867474addbf4f2997
Marzia Favaro <maarzia.f@gmail.com>
2019-05-23 00:28:45 +0200

    fixed error in structure

--- hand_segmentation/test.py
+++ hand_segmentation/test.py
@@ -20,10 +20,10 @@ from os import path as op
 
 # Network components
 def conv_block(input_tensor, num_filters):
-    encoder = Conv2D(num_filters, (3, 3), padding='same')(input_tensor)
+    encoder = Conv2D(num_filters, (2, 2), padding='same')(input_tensor)
     encoder = BatchNormalization()(encoder)
     encoder = Activation('relu')(encoder)
-    encoder = Conv2D(num_filters, (3, 3), padding='same')(encoder)
+    encoder = Conv2D(num_filters, (2, 2), padding='same')(encoder)
     encoder = BatchNormalization()(encoder)
     encoder = Activation('relu')(encoder)
     return encoder
@@ -31,7 +31,7 @@ def conv_block(input_tensor, num_filters):
 
 def encoder_block(input_tensor, num_filters):
     encoder = conv_block(input_tensor, num_filters)
-    encoder_pool = MaxPooling2D((2, 2), strides=(2, 2))(encoder)
+    encoder_pool = MaxPooling2D((3, 3), strides=(2, 2))(encoder)
     return encoder_pool, encoder
 
 
@@ -40,10 +40,10 @@ def decoder_block(input_tensor, concat_tensor, num_filters):
     decoder = concatenate([concat_tensor, decoder], axis=-1)
     decoder = BatchNormalization()(decoder)
     decoder = Activation('relu')(decoder)
-    decoder = Conv2D(num_filters, (3, 3), padding='same')(decoder)
+    decoder = Conv2D(num_filters, (2, 2), padding='same')(decoder)
     decoder = BatchNormalization()(decoder)
     decoder = Activation('relu')(decoder)
-    decoder = Conv2D(num_filters, (3, 3), padding='same')(decoder)
+    decoder = Conv2D(num_filters, (2, 2), padding='same')(decoder)
     decoder = BatchNormalization()(decoder)
     decoder = Activation('relu')(decoder)
     return decoder
@@ -57,7 +57,7 @@ mini_root_path = op.join(data_root_path, 'mini')
 mini_all_features_path = op.join(mini_root_path, 'all_features')
 mini_target_path = op.join(mini_root_path, 'target')
 
-# Get data/home/marzia
+# Get data
 print("Getting data")
 mini_train = sio.loadmat(op.join(mini_all_features_path, 'features_train.mat')).get('features_train')
 mini_target = sio.loadmat(op.join(mini_target_path, 'target_train.mat')).get('target_train')
@@ -76,14 +76,14 @@ epochs = 3  # random choice
 # Network
 inputs = Input(shape=img_shape)
 
-encoder0_pool, encoder0 = encoder_block(inputs, 8)
-encoder1_pool, encoder1 = encoder_block(inputs, 16)
+encoder0_pool, encoder0 = encoder_block(inputs, 16)
+encoder1_pool, encoder1 = encoder_block(encoder0_pool, 32)
 print("Encoders created")
-center = conv_block(encoder1_pool, 32)
+center = conv_block(encoder1_pool, 64)
 print("Center created")
-decoder1 = decoder_block(center, encoder1, 16)
-decoder0 = decoder_block(decoder1, encoder0, 8)
-# print("Decoders created")
+decoder1 = decoder_block(center, encoder1, 32)
+decoder0 = decoder_block(decoder1, encoder0, 16)
+print("Decoders created")
 
 # outputs = Conv2D(1, (1, 1), activation='sigmoid')(decoder0)
 
