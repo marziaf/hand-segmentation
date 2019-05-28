2fcb1e2c97bd91a0c9236d0d5ebfca3d6e56ce8c
Marzia Favaro <maarzia.f@gmail.com>
2019-05-28 22:48:25 +0200

    Back to array implementation instead of tf.data

--- source/hand_segmentation/data_generator.py
+++ source/hand_segmentation/data_generator.py
@@ -16,7 +16,7 @@ import random
 # Uploads data from mat file to numpy array
 # Reduce arrays sizes by reduce_images and reduction_factor
 # Returns features, labels as numpy arrays
-def __get_np_array(feat_path, lab_path, feat_variable='features', lab_variable='labels',
+def get_data(feat_path, lab_path, feat_variable='features', lab_variable='labels',
                    reduce_images=False, reduction_factor=0.3):
     # Import data
     print("Importing data")
@@ -37,37 +37,23 @@ def __get_np_array(feat_path, lab_path, feat_variable='features', lab_variable='
     return features, labels
 
 
-# Returns a tensorflow.data
-# TODO is this passage of functions a waste of memory?
-def get_data(feat_path, lab_path, reduce_set=False, reduction_factor=0.3):
-    features, labels = __get_np_array(feat_path, lab_path, reduce_images=reduce_set,
-                                      reduction_factor=reduction_factor)
-    dataset = tf.data.Dataset.from_tensor_slices((features, labels))
-    # TODO shuffle...
-    return dataset
-
-
-# Displays random images couples of data1 and data2
-def disp_data_comparison(dataset, num_rows=3, num_cols=3):
-
-    data_iter = data.make_one_shot_iterator()
-    next_element = data_iter.get_next()
+def disp_some_data(feat, lab):
 
     fig = plt.figure(figsize=(20, 10))
-    outer = gridspec.GridSpec(num_rows, num_cols, wspace=0.2, hspace=0.2)
+    outer = gridspec.GridSpec(3, 3, wspace=0.2, hspace=0.2)
+    tot_images = int(feat.shape[0])
 
-    for i in range(num_rows*num_cols):
-        imgs, labs = tf.keras.backend.get_session().run(next_element)
-        img = imgs[0]
-        lab = labs[0]
-        inner = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=outer[i], wspace=0.1, hspace=0.1)
+    for i in range(9):
+        inner = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=outer[i],
+                                                 wspace=0.1, hspace=0.1)
+        inx = random.randint(0, tot_images-1)
 
         ax = fig.add_subplot(inner[0])
-        ax.imshow(img)
+        ax.imshow(feat[inx, :, :, :3])
         ax.axis('off')
         ax = fig.add_subplot(inner[1])
         ax.axis('off')
-        ax.imshow(lab)
+        ax.imshow(lab[inx, :, :, 0])
         fig.add_subplot(ax)
 
     fig.show()
@@ -82,6 +68,5 @@ features_path = op.join(sets_root_path, 'features.mat')
 labels_path = op.join(sets_root_path, 'labels.mat')
 
 # TEST
-# f, l = __get_np_array(features_path, labels_path, reduce_images=True)
-data = get_data(features_path, labels_path)
-disp_data_comparison(data)
+f, l = get_data(features_path, labels_path, reduce_images=True)
+disp_some_data(f, l)
