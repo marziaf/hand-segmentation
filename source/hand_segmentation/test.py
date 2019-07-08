fac2007b939cb0b74ae483de9c1549d356ea8043
Marzia Favaro <maarzia.f@gmail.com>
2019-07-08 22:16:34 +0200

    test asks user which model to load

--- source/hand_segmentation/test.py
+++ source/hand_segmentation/test.py
@@ -1,17 +1,26 @@
-from paths import save_model_path, test_features_path, test_labels_path, save_output_images_path
+from paths import save_model_dir, test_features_path, test_labels_path, save_output_images_path
+from os import path as op
 from data_generator import get_data, disp_some_data
 from network import ce_dice_loss
 import keras
 from keras import models, Model, utils
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model")
+args = parser.parse_args()
+model = args.model
+
+model_path = op.join(save_model_dir, model)
 
 # %% Load model
 print("Loading model")
-model = models.load_model(save_model_path, custom_objects={'ce_dice_loss': ce_dice_loss})
+model = models.load_model(model_path, custom_objects={'ce_dice_loss': ce_dice_loss})
 # %% Image acquisition
 print("Getting test images")
-test_features = np.load(test_features_path)
-test_features = test_features[:10]  # TODO remove if memory saving is not needed
+test_features = np.load(model_path)
+test_features = test_features[:30]  # TODO remove if memory saving is not needed
 # test_labels = np.load(test_features_path)
 # print("Getting categorical version of labels")
 # test_labels = utils.to_categorical(test_labels)
